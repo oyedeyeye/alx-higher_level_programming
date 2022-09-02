0c93ef5f27fa141b13984a22353a8d3cd0ce0006
Hezekiah Oyemade <oyedey@gmail.com>
2022-09-02 17:44:48 +0100

    Roman Numeral to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,35 @@
+def roman_to_int(roman_string):
+    roman_numerals = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1
+    }
+    temp = []
+    result = 0
+
+    for i in roman_string:
+        temp.append(roman_numerals[i])
+    if len(temp) == 1:
+        result += temp[0]
+        return result
+
+    j = 0
+    while j < len(temp):
+        if temp[j] == temp[len(temp) - 1]:
+            result += temp[j]
+        elif temp[j] >= temp[j + 1]:
+            result += temp[j]
+        else:
+            result -= temp[j]
+        j += 1
+    return result
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     for i in a_dictionary:
-        if i == key: 
+        if i == key:
             del a_dictionary[key]
             return a_dictionary
     return a_dictionary
