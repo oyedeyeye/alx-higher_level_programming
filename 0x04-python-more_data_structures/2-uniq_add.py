385c9e4d1a28ed867dc754ee556cbfd67a905c1b
Hezekiah Oyemade <oyedey@gmail.com>
2022-09-05 14:34:12 +0100

    new update - unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list is not []:
+    if my_list != []:
         sum = 0
         unique_list = list(set(my_list))
-        print(unique_list)
         for i in unique_list:
             sum += i
         return sum
