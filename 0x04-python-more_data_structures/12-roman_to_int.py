6e85c14a7a8b04f6992b0a2100a8b5b214fbf59c
Hezekiah Oyemade <oyedey@gmail.com>
2022-09-05 16:26:12 +0100

    new update - roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if len(roman_string) is not None:
-        roman_numerals = {
-            "M": 1000,
-            "CM": 900,
-            "D": 500,
-            "CD": 400,
-            "C": 100,
-            "XC": 90,
-            "L": 50,
-            "XL": 40,
-            "X": 10,
-            "IX": 9,
-            "V": 5,
-            "IV": 4,
-            "I": 1
-        }
-        temp = []
-        result = 0
-
-        for i in roman_string:
-            temp.append(roman_numerals[i])
-        if len(temp) == 1:
-            result += temp[0]
-            return result
-
-        j = 0
-        while j < len(temp):
-            if temp[j] == temp[len(temp) - 1]:
-                result += temp[j]
-            elif temp[j] >= temp[j + 1]:
-                result += temp[j]
-            else:
-                result -= temp[j]
-            j += 1
-        return result
-    else:
+    if not roman_string:
         return 0
+    if not isinstance(roman_string, str):
+        return 0
+    if not roman_string.isupper():
+        return 0
+
+    roman_numerals = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1
+    }
+    temp = []
+    result = 0
+    for i in roman_string:
+        temp.append(roman_numerals[i])
+    if len(temp) == 1:
+        result += temp[0]
+        return result
+    j = 0
+    while j < len(temp):
+        if temp[j] == temp[len(temp) - 1]:
+            result += temp[j]
+        elif temp[j] >= temp[j + 1]:
+            result += temp[j]
+        else:
+            result -= temp[j]
+        j += 1
+    return result
