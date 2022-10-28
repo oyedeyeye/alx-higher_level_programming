83ce19eda1afb811c341be983883fcf93ef15d29
Hezekiah Oyemade <oyedey@gmail.com>
2022-10-28 12:57:11 +0100

    task 3 updated

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except (ZeroDivisionError, ValueError, TypeError):
         result = None
     finally:
         print("Inside result: {}".format(result))
