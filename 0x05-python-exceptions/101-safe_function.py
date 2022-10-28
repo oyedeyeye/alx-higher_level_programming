1b0ce6d3519a6575bbd0a7c2aade026760928ee4
Hezekiah Oyemade <oyedey@gmail.com>
2022-10-28 16:41:59 +0100

    task 101 updated pycodestyles

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@
 # You can assume fct will be always a pointer to a function
 # Returns the result of the function,
 # Otherwise, returns None if something happens during the function
-#and prints in stderr the error precede by Exception:
+# and prints in stderr the error precede by Exception:
 # You have to use try: / except:
 import sys
 
