15f6d7c654ef2bf0de1272742ca3dd37b59e54b1
Hezekiah Oyemade <oyedey@gmail.com>
2022-10-28 16:42:23 +0100

    task 102 updated pycodestyles

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -10,7 +10,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-            except:
+            except (ValueError, TypeError, IndexError, ZeroDivisionError):
                 result = b + a
                 break
     return result
