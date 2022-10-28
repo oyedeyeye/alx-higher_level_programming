2bbc45b84c1bb8a0133dafd9d94c2d89215983e7
Hezekiah Oyemade <oyedey@gmail.com>
2022-10-28 14:43:00 +0100

    task 100 updated pycodestyles

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-#function that prints an integer.
+# function that prints an integer.
 # Prototype: def safe_print_integer_err(value):
 # value can be any type (integer, string, etc.)
 # The integer should be printed followed by a new line
-# Returns True if value has been correctly printed 
+# Returns True if value has been correctly printed
 # (it means the value is an integer)
-# Otherwise, returns False 
+# Otherwise, returns False
 # and prints in stderr the error precede by Exception:
 # You have to use try: / except:
 # You have to use "{:d}".format() to print as integer
 # You are not allowed to use type()
 import sys
 
+
 def safe_print_integer_err(value):
+
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as t_v_err:
