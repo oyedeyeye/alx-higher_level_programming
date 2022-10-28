62677d7831f343c09399822f6a2df7bb50f41c70
Hezekiah Oyemade <oyedey@gmail.com>
2022-10-28 15:17:32 +0100

    task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# function that executes a function safely.
+# Prototype: def safe_function(fct, *args):
+# You can assume fct will be always a pointer to a function
+# Returns the result of the function,
+# Otherwise, returns None if something happens during the function
+#and prints in stderr the error precede by Exception:
+# You have to use try: / except:
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except (ValueError, TypeError, IndexError, ZeroDivisionError) as many_Err:
+        result = None
+        print("Exception:", many_Err, file=sys.stderr)
+        return result
+    else:
+        return result
