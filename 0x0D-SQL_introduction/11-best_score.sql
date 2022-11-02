7326935a6bdab880b039ff16ab766f185d207c01
Hezekiah Oyemade <oyedey@gmail.com>
2022-11-01 18:11:12 -0700

    task 11 updated

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -2,5 +2,4 @@
 -- second_table of the database hbtn_0c_0 in your MySQL server.
 -- Results should display both the score and the name (in this order)
 -- Records should be ordered by score (top first)
-SELECT score, name FROM second_table GROUP BY score WHERE score >= 10
-ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
