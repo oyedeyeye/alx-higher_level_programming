1d0287e6fccce01f56b465a9d301831748c407a1
Hezekiah Oyemade <oyedey@gmail.com>
2022-11-03 03:21:40 +0100

    advanced task

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,14 @@
+-- Import the database dump from hbtn_0d_tvshows_rate to your MySQL server: download (same as 102-rating_shows.sql)
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY name
+ORDER BY rating DESC;
