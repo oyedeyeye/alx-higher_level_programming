5e77d9f2cb780b61073e3a5e91f92ea829cd5334
Hezekiah Oyemade <oyedey@gmail.com>
2023-01-12 16:13:06 +0100

    add coordinates for the square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -99,5 +99,5 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
-                print(' ' * self.__position[0], end='')
-                print('#' * self.__size)
+                print("\n" * self.__position[1], end='')
+                print(' ' * self.__position[0] + '#' * self.__size)
