5bf1707f24ae5fe23879c08625b2eefc73da069e
Hezekiah Oyemade <oyedey@gmail.com>
2023-02-15 13:27:14 +0100

    Lazy matrix multiplication with numpy updated

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,4 +9,5 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Function that multiplies 2 matrices using the numpy module"""
-    return np.dot(m_a, m_b)
+    # return np.dot(m_a, m_b)
+    return np.matmul(m_a, m_b)
