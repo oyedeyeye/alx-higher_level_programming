0a8d21d27ffa67d54ca4a9c922de7c3fc287aed9
Hezekiah Oyemade <oyedey@gmail.com>
2023-02-23 17:55:00 +0100

    square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module 10: Square subclass
+inherits from Rectangle that inherits from BaseGeometry
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle
+
+    Methods:
+        __init__(self, size)
+    """
+
+    def __init__(self, size):
+        """initializes size
+
+        Args:
+            size (int): private attribute"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """returns string with square description"""
+        return "[{:s} {:d}/{:d}]".format(self.__class__.__name__,
+                                         self.__size, self.__size)
