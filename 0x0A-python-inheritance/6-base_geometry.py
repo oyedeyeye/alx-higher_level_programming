3643dc4d55eddcbd570ad93300d3e264bb39fbb4
Hezekiah Oyemade <oyedey@gmail.com>
2023-02-23 11:12:11 +0100

    Improved Geometry module - task 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,15 +4,14 @@ with public instance method"""
 
 
 class BaseGeometry:
-    """Class with public instance method 
+    """Class with public instance method
 
     Methods:
         area
     """
 
     def __init__(self, size=0, width=0):
-        self.size = size
-        self.width = width
+        pass
 
     def area(self):
         raise Exception("area() is not implemented")
