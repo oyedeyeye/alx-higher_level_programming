a556d6236dc925bf371e451146ba794789a502ad
Hezekiah Oyemade <oyedey@gmail.com>
2023-02-23 12:14:23 +0100

    Integer validator Geometry module - task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module 6: Base geometry
+with public instance methods"""
+
+
+class BaseGeometry:
+    """Class with public instance method
+
+    Methods:
+        area: computes area of the geometry
+        integer_validator: validates integer values
+    """
+
+    def __init__(self, size=0, width=0):
+        pass
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if not isinstance(self.value, int):
+            raise TypeError("{:s} must be an integer".format(self.name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(self.name))
+
+    def area(self):
+        raise Exception("area() is not implemented")
