3ff00f8ce204d98dddbc6ec4050f37a392abe511
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-09 11:01:24 +0100

    student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module 9: student
+Class Student that defines student
+
+methods:
+        def __init__(self, first_name, last_name, age)
+        def to_json(self)"""
+
+
+class Student:
+    """student attributes
+
+        Public Instance Attributes:
+        first_name (str): string type input
+        last_name (str): string type input
+        age (int): age in years
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """instantiate the class
+
+            Args:
+                first_name (str): string type input
+                last_name (str): string type input
+                age (int): age in years
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve dictionary representation of Student instance"""
+        if attrs is not None:
+            dic = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    dic[attr] = self.__dict__[attr]
+            return dic
+        return self.__dict__
