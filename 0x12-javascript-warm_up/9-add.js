9bd8ebb75140e678149e946557b401ad7fd8f49f
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 15:26:49 +0100

    task 9: add function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that prints the addition of 2 integers
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+console.log(add(process.argv[2], process.argv[3]));
