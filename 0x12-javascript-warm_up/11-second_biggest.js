fa54b915427448e0ec374fca2460c4b0728782e1
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 16:53:24 +0100

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that searches the second biggest integer in the list of arguments.
+function secondBiggest (arr) {
+  if (arr === undefined || arr.length === 0) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  }
+  const sortedArr = [...arr];
+  sortedArr.sort((a, b) => b - a);
+  return sortedArr[1];
+}
+
+const args = process.argv.slice(2);
+console.log(secondBiggest(args));
