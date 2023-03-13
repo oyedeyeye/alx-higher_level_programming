4a1de7a3e11d09a9abc2800bc21c8a52865c9545
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 22:43:51 +0100

    additional task 101: loop function

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // function that executes x times a function
-function callMeMoby(x, theFunction) {
-	let i = 0;
-	while (i < x) {
-		theFunction();
-		i += 1;
-	}
+function callMeMoby (x, theFunction) {
+  let i = 0;
+  while (i < x) {
+    theFunction();
+    i += 1;
+  }
 }
 
 module.exports = { callMeMoby };
