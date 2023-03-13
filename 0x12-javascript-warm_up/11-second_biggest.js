54dc77602e727fabc10ad0179ab73845ffe363f6
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 17:30:41 +0100

    task 11: second biggest number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ function secondBiggest (arr) {
   if (arr === undefined || arr.length === 0) {
     return 0;
   } else if (arr.length === 1) {
-    return arr[0];
+    return 0;
   }
   const sortedArr = [...arr];
   sortedArr.sort((a, b) => b - a);
