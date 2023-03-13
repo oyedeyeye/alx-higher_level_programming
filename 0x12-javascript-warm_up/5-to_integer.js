9b9ac12f227f50e50298da732d9fe9432ea352a6
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 17:26:08 +0100

    task 5: integer parsing

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const intArg = parseInt(args[0]);
 if (Number.isInteger(intArg)) {
-  console.log(intArg);
+  console.log('My number: ' + intArg);
 } else {
   console.log('Not a number');
 }
