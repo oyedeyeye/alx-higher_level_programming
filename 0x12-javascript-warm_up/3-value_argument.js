05ca347269c67013f2a0d88781d71eca3f258331
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 14:10:06 +0100

    task 3: value of an argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// script that prints the first argument passed to it
+
+const args = process.argv;
+if (args[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[2]);
+}
