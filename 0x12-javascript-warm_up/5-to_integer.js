e25cddafb9f55d8b880fe91b23805e668bf0237f
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-13 14:44:41 +0100

    task 5: integer conversion

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const intArg = parseInt(args[0]);
+if (Number.isInteger(intArg)) {
+  console.log(intArg);
+} else {
+  console.log('Not a number');
+}
