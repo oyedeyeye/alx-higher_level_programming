d03f06bd1711abf6fd320377918ba17a00d4dbab
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-14 15:16:32 +0100

    task 4: Rectangle methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let row = '';
+      while (j < this.width) {
+        row += 'X';
+        j += 1;
+      }
+      console.log(row);
+      i += 1;
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
