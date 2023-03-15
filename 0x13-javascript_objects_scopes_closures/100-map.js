f905faf8601472959403a46cb4b770dfc11f3436
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-15 03:06:31 +0100

    task 100: factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-const newList = [...list].map(el => el * list.indexOf(el));
-console.log(list);
+const data = list;
+const newList = data.map(el => el * data.indexOf(el));
+console.log(data);
 console.log(newList);
