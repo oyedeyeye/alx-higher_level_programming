ac6b1c0daa7d72b6c9484b41f8b4d5e5e090a512
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-20 00:53:50 +0100

    Task 0: Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+"""
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ = "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3])
+
+    cur = conn.cursor
+
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_results = cur.fetchall()
+    for i in query_results:
+        print(i)
+
+    cur.close()
+    conn.close()
