714d56baf49ed9172f0156cdd342631fb40be1de
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-20 11:58:24 +0100

    Task 1: filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-^#!/usr/bin/python3
+#!/usr/bin/python3
 """Module 1: filter_states
 script that lists all states with a name
 starting with N (upper N) from the database `hbtn_0e_0_usa`
@@ -17,9 +17,10 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     # SQL statement execution
-    cur.execute("""SELECT * FROM states
+    cur.execute(
+            """SELECT * FROM states
                     WHERE name LIKE 'N%'
-                    ORDER BY states.id ASC""")
+                    ORDER BY states.id ASC;""")
     query_results = cur.fetchall()
     for i in query_results:
         print(i)
