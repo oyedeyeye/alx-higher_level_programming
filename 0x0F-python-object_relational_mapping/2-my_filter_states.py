27551688b7bed79e36af9a02ed00373de0887fb7
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-20 13:05:09 +0100

    Task 2: filter by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module 2: my_filter_states
+script that takes in an argument and displays all values in the
+states table of `hbtn_0e_0_usa` where name matches the argument.
+should take 4 arguments: `mysql username`,
+                          `mysql password`,
+                          `database name` and 
+                          `state name searched` (no argument validation needed)
+"""
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Database connection
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    # SQL statement execution
+    cur.execute("""SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY states.id ASC""".format(argv[4]))
+    query_results = cur.fetchall()
+    [print(row) for row in query_results]
+
+    cur.close()
+    db.close()
