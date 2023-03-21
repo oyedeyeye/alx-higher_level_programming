be329ee06877e6310557777b470e4c97718ecf3e
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-21 17:36:57 +0100

    task 14: Cities in states - Relationship

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -25,11 +25,9 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in (
-        session.query(City, State)
-        .filter(City.state_id == State.id)
-        .order_by(City.id)
-        .all()):
+    for city, state in (session.query(City, State)
+                        .filter(City.state_id == State.id)
+                        .order_by(City.id).all()):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
