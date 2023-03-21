03408fc1903b51db45b3d794af5480bd1b983ecc
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-21 17:31:59 +0100

    task 14: Cities in states - Relationship

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module 14: model_city_fetch_by_state
+script that  prints all `City` objects from the database `hbtn_0e_14_usa`
+
+Your script should take 3 arguments: `<mysql username>`,
+                                       `<mysql password>` and
+                                       `<database name>`
+You must use the module SQLAlchemy
+"""
+
+
+from sys import argv
+from model_state import Base, State
+from model_city import City
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    # Connection to Database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city, state in (
+        session.query(City, State)
+        .filter(City.state_id == State.id)
+        .order_by(City.id)
+        .all()):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module 14: model_city
+Script that contains the class definition of a `City`
+and an instance Base = declarative_base():
+
+City class:
+    inherits from Base
+    links to the MySQL table `cities` Using `SQLAlchemy`
+    class attribute `id` that represents a column of an auto-generated,
+    unique integer, can’t be null and is a primary key
+    class attribute `name` that represents a column of a string with
+    maximum 128 characters and cant be null
+    class attribute state_id that represents a column of an integer,
+    cant be null and is a foreign key to states.id
+"""
+
+
+import model_state
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+# Declarative base class
+# Base = declarative_base()
+
+
+# City inheiting from model_state Base class
+class City(model_state.Base):
+    """Inherits from the model_state' Base class and
+    Links to the MySQL table `cities`
+
+    Attributes:
+        __tablename__ (str): name of MySQL table to store cities
+        id (sqlalchemy.Integer): The city's id, a primary key
+        name (sqlalchemy.String): the name of the city
+        state_id = A foreign key relating to model_state.State.id
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"),
+                      nullable=False)
