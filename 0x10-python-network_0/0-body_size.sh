b3879ae32f981684571743bcde3b447966be5f44
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-30 18:28:56 +0100

    task 0 updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays
-curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
+curl -sI "$1" | grep 'Content-Length:' | cut -d ' ' -f2
