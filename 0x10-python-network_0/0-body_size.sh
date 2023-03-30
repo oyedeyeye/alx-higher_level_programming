732a4879322ef83f2517148aa5433f3bed878b8e
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-30 16:44:42 +0100

    task 0: cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # script that takes in a URL, sends a request to that URL, and displays
 # the size of the body of the response
 # The size must be displayed in bytes
-curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
