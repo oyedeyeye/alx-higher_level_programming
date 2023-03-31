92bf361a58cb13ace57407810ac7e026f2eda791
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-31 13:31:32 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,9 @@ using the package urllib
 """
 
 
+import urllib.request
+
 if __name__ == '__main__':
-    import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
