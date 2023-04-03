8ef98051dcbaeb2d9da78c32493b8fa56e50c5a9
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-03 21:14:22 +0100

    task 3: Error Code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+manage urllib.error.HTTPError exceptions and print:
+Error code: followed by the HTTP status code
+"""
+
+
+import sys
+import urllib.request
+
+
+if __name__ = '__main__':
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as res:
+            content = res.read()
+            print(content.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
