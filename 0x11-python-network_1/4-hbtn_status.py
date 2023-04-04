fab65a426f85c0d9f495198597082d839f2f88b0
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-04 03:28:07 +0100

    task 4: request status updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,6 +10,5 @@ if __name__ == '__main__':
 
     res = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(res)))
+    print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
-    print("\t- utf8 content: {}".format((res.decode('utf-8'))))
