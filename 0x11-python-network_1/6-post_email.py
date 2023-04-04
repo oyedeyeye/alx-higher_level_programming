fef77ecf4c3e33ef19e1adef3d4526bb6a56c543
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-04 18:14:54 +0100

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to the passed
+URL with email as a parameter, displays body of response
+The email must be sent in the email variable
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    payload = {"email": sys.argv[2]}
+    req = requests.post(url, data=payload)
+    print("Your email is: {}".format(req.text.email))
