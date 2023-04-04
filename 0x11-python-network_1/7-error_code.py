d93d32e9a5d81d4b9364887f34a5ebd274153b2a
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-04 18:29:28 +0100

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays the
+body of the response. If the HTTP status code is greater than or equal to 400,
+print: Error code: followed by the value of the HTTP status code
+use the packages requests and sys
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code() >= 400:
+        print("Error code: {}".format(req.status_code()))
