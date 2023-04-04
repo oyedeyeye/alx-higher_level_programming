05afa4b257142e717355489a9d319fb09a35316c
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-04 18:40:24 +0100

    task 7 updated

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
     req = requests.get(url)
-    if req.status_code() >= 400:
-        print("Error code: {}".format(req.status_code()))
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
