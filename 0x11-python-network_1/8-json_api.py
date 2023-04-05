9857f676f90e7305fb6fad187ebc8942494461f7
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-05 13:07:56 +0100

    task 8 new correction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
     url = 'http://0.0.0.0:5000/search_user'
 
-    if len(sys.argv[1]) == 1:
+    if len(sys.argv) == 1:
         q = ""
     else:
         q = sys.argv[1]
