2fe300fa91b3ba3731114e6db893474abbaf4e0f
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-05 14:04:48 +0100

    task 9: my github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id. Use Basic Authentication with a
+personal access token as password to access your information (only read:user
+permission is needed). first argument will be your username, second argument
+will be your password (in your case, a personal access token as password)
+"""
+
+
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == '__main__':
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    basic = HTTPBasicAuth(username, password)
+
+    url = 'https://api.github.com/users'
+
+    req = requests.get(url, auth=basic)
+    result = req.json()
+
+    if result.get('authenticated') == True:
+        print(result.get('id'))
+    else:
+        print("None")
