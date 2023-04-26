253a057300e5fce1f0fe1e2c627c2d215cdb70e9
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-26 14:02:20 +0100

    task 2: request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -9,5 +9,5 @@ const request = require('request');
 const urlArg = process.argv[2];
 
 request.get(urlArg).on('response', response => {
-	console.log('code: ', response.statusCode);
+  console.log('code: ', response.statusCode);
 });
