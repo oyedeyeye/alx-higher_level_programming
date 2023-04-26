b2437533df25d7a2e3a6ff61fefb4a3b0095fa5b
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-26 14:03:10 +0100

    task 2: request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-/**
- * script that display the status code of a GET request.
- * The first argument is the URL to request (GET)
- * The status code must be printed like this: code: <status code>
- * You must use the module request
- */
 const request = require('request');
 const urlArg = process.argv[2];
 
