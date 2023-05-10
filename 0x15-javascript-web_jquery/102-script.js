e807c06886a67aae1e19fc9b322f1af4722c76c0
Hezekiah Oyemade <oyedey@gmail.com>
2023-05-10 19:21:59 +0100

    advanced task 102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,5 +1,5 @@
 $('document').ready(function () {
-  const api = 'https://fourtonfish.com/hellosalut/?lang=fr';
+  const api = 'https://fourtonfish.com/hellosalut/hello';
   $('INPUT#btn_translate').click(function () {
     $.get(api + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
       $('DIV#hello').html(data.hello);
