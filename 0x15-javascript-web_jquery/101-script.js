585dd20980d5e902191db556879d6bd8e6181baa
Hezekiah Oyemade <oyedey@gmail.com>
2023-05-10 15:15:59 +0100

    task 101 list, add, remove

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('DIV#add_item').on('click', function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').on('click', function () {
+    $('UL.my_list li').last().remove();
+  });
+  $('DIV#clear_list').on('click', function () {
+    $('UL.my_list').empty();
+  });
+});
